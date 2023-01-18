1dfb05ba436ac87f671ea629e9f8f451fda3b91c
idfilatov-stl <122741014+idfilatov-stl@users.noreply.github.com>
2023-01-18 12:21:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 from fastapi import FastAPI
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
-
+load_dotenv()
 if __name__ == '__main__':
     print('hello')
+    POSTGRES_USER = os.getenv('POSTGRES_USER')
+    POSTGRES_DB = os.getenv('POSTGRES_DB')
+    POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
+    POSTGRES_HOST = os.getenv('POSTGRES_HOST')
+    engine = create_engine(
+        f'postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:5432/{POSTGRES_DB}'
+    )
+    print(engine)
