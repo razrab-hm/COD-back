57244526b87a16d1f6c80aab25eab634578daef1
eugene <Eugene.stlab@gmail.com>
2023-01-30 19:24:20 +0200

    docker

--- tests/registration.py
+++ tests/registration.py
@@ -9,7 +9,7 @@ def test(username, password, email):
           "password": password,
           "email": email
             }
-    return requests.post('http://localhost:8000/users/', data=json.dumps(data)).text
+    return requests.post('http://127.0.0.1:8000/users/', data=json.dumps(data)).text
 
 
 print(requests.get('https://google.com').text)
