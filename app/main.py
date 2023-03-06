9b91ec78be6333bf1ecf6f948edda9aa22b4e4a5
eugene <Eugene.stlab@gmail.com>
2023-03-06 16:47:43 +0200

    pipeline

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request, APIRouter
+from fastapi import FastAPI, Request
 from fastapi_jwt_auth.exceptions import AuthJWTException
 from starlette.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
@@ -11,14 +11,11 @@ from app.routs.token import router as token_router
 from app.routs.reports import router as report_router
 
 app = FastAPI()
-router = APIRouter(prefix='/api', tags=["api"])
-app.include_router(router)
-router.include_router(user_router)
-router.include_router(company_router)
-router.include_router(hash_router)
-router.include_router(token_router)
-router.include_router(report_router)
-
+app.include_router(user_router, prefix="/api")
+app.include_router(company_router, prefix="/api")
+app.include_router(hash_router, prefix="/api")
+app.include_router(token_router, prefix="/api")
+app.include_router(report_router, prefix="/api")
 
 origins = [
     "https://localhost",
