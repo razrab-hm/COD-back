75f1dc86f1b2e0243128d13d5426d03a413737c5
eugene <Eugene.stlab@gmail.com>
2023-03-20 13:39:35 +0200

    cicd

--- app/routs/hashrates.py
+++ app/routs/hashrates.py
@@ -55,8 +55,8 @@ def save_upload(company_id: int, hashrate_list: list = Body(...), auth: AuthJWT 
 
 @router.delete('/company/{company_id}')
 def delete_hashrates_by_dates(company_id: int,
-                              from_date: date = Body(...),
-                              to_date: date = Body(...),
+                              from_date: date = Query(...),
+                              to_date: date = Query(...),
                               db: Session = Depends(get_db),
                               auth: AuthJWT = Depends()):
     return handlers.delete_hashrates_by_dates_handler(company_id, db, auth, from_date, to_date)
